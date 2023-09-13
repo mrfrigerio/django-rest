c2044ed5a80941cb8ebb611f025cad7172d6360e
Marcelo Ragnelli Frigerio <mrfrigerio@yahoo.com.br>
2023-09-12 22:57:43 -0300

    Utilizando a view do DRF

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,17 @@
-import json
-from django.http import JsonResponse, HttpRequest
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.request import HttpRequest
+from rest_framework.decorators import api_view
+
+from products.models import Product
+from products.serializers import ProductSerializer
 
 
+@api_view(["GET", "POST"])
 def api_home(request: HttpRequest, *args, **kwargs):
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(request.body)  # Semelhante ao JSON.parse do Js
-        print(json.dumps(request.GET))  # Semelhante ao JSON.stringify do Js
-    except:
-        pass
-    print(data)
-    return JsonResponse(data)
+    if instance:
+        # data = model_to_dict(instance, fields=["id", "title", "price", "sale_price"])
+        data = ProductSerializer(instance).data
+    return Response(data)
